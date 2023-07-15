2ef47006778743f9fc5e0b99882f316bca846eef
A.B. Rakib <96842069+abrakib942@users.noreply.github.com>
2023-07-16 00:22:45 +0600

    book api

--- frontend/src/redux/api/apiSlice.ts
+++ frontend/src/redux/api/apiSlice.ts
@@ -2,6 +2,8 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const api = createApi({
   reducerPath: "api",
-  baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:5000" }),
+  baseQuery: fetchBaseQuery({
+    baseUrl: "https://book-catalog-orcin.vercel.app",
+  }),
   endpoints: () => ({}),
 });
---
+++ frontend/src/redux/features/book/bookApi.ts
@@ -0,0 +1,15 @@
+/* eslint-disable @typescript-eslint/restrict-template-expressions */
+import { api } from "../../api/apiSlice";
+
+const bookApi = api.injectEndpoints({
+  endpoints: (builder) => ({
+    getBooks: builder.query({
+      query: () => "/books",
+    }),
+    getSingleBook: builder.query({
+      query: (id) => `/book/${id}`,
+    }),
+  }),
+});
+
+export const { useGetBooksQuery, useGetSingleBookQuery } = bookApi;
