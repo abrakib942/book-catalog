2517b234ae401a789dd2015838181685f35089c6
A.B. Rakib <96842069+abrakib942@users.noreply.github.com>
2023-07-16 21:38:02 +0600

    filter and search

--- frontend/src/pages/AllBooks.tsx
+++ frontend/src/pages/AllBooks.tsx
@@ -7,8 +7,33 @@ import { IBook } from "../types/globalTypes";
 import Card from "../components/Card";
 import Loading from "../components/Loading";
 import Navbar from "../layouts/Navbar";
+import { Link } from "react-router-dom";
 
 const AllBooks = () => {
+  const genres = [
+    "Mystery",
+    "Fantasy",
+    "Science Fiction",
+    "Historical Fiction",
+    "Poetry",
+  ];
+
+  const publicationYears = [
+    "2011",
+    "2012",
+    "2013",
+    "2014",
+    "2015",
+    "2016",
+    "2017",
+    "2018",
+    "2019",
+    "2020",
+    "2021",
+    "2022",
+    "2023",
+  ];
+
   const [selectGenre, setSelectGenre] = useState("");
   const [searchText, setSearchText] = useState("");
   const [selectpublicationYear, setSelectPublicationYear] = useState("");
@@ -32,7 +57,13 @@ const AllBooks = () => {
         <div className="w-1/4 bg-gray-200 p-4">
           <div className="flex items-center justify-between mb-4">
             <h2 className="text-lg font-semibold">Filters</h2>
-            <button className="text-[14px] bg-green-500 text-white px-[12px] py-[4px] rounded-[8px]">
+            <button
+              onClick={() => {
+                setSelectGenre("");
+                setSelectPublicationYear("");
+              }}
+              className="text-[14px] bg-green-500 text-white px-[12px] py-[4px] rounded-[8px]"
+            >
               Reset
             </button>
           </div>
@@ -41,15 +72,23 @@ const AllBooks = () => {
             <div className="bg-white p-2 rounded">
               <h2 className="text-[15px] text-gray-400">By Genre:</h2>
               <div className="mt-2">
-                <div className="flex items-center mb-[8px]">
-                  <input
-                    className="h-[18px] w-[18px]"
-                    id=""
-                    type="radio"
-                    name="genre"
-                  />
-                  <label className="text-[14px] ml-3" htmlFor=""></label>
-                </div>
+                {genres?.map((genre, i) => {
+                  return (
+                    <div key={i} className="flex items-center mb-[8px]">
+                      <input
+                        onChange={() => setSelectGenre(genre)}
+                        className="h-[18px] w-[18px]"
+                        id={genre}
+                        type="radio"
+                        name="genre"
+                        checked={selectGenre === genre}
+                      />
+                      <label className="text-[14px] ml-3" htmlFor={genre}>
+                        {genre}
+                      </label>
+                    </div>
+                  );
+                })}
               </div>
             </div>
             <div className="bg-white p-2 rounded">
@@ -58,15 +97,23 @@ const AllBooks = () => {
                   By Publication Year:
                 </h2>
                 <div className="mt-2">
-                  <div className="flex items-center mb-[8px]">
-                    <input
-                      className="h-[18px] w-[18px]"
-                      id=""
-                      type="radio"
-                      name="genre"
-                    />
-                    <label className="text-[14px] ml-3" htmlFor=""></label>
-                  </div>
+                  {publicationYears?.map((year, i) => {
+                    return (
+                      <div key={i} className="flex items-center mb-[8px]">
+                        <input
+                          onChange={() => setSelectPublicationYear(year)}
+                          className="h-[18px] w-[18px]"
+                          id={year}
+                          type="radio"
+                          name="year"
+                          checked={selectpublicationYear === year}
+                        />
+                        <label className="text-[14px] ml-3" htmlFor={year}>
+                          {year}
+                        </label>
+                      </div>
+                    );
+                  })}
                 </div>
               </div>
             </div>
@@ -78,16 +125,13 @@ const AllBooks = () => {
           {/* Search bar */}
           <div className="flex items-center mb-4">
             <input
+              onChange={(e) => setSearchText(e.target.value)}
               className="border border-gray-300 p-2 mr-2 flex-grow"
               type="text"
               placeholder="Search..."
             />
-            <button className="bg-green-500 text-white py-2 px-4 rounded">
-              Search
-            </button>
           </div>
 
-          {/* All Books */}
           <div className="mt-[20px] mb-[100px]">
             <div className="grid grid-cols-3 gap-x-10 gap-y-10">
               {bookData?.data?.map((book: IBook, i: number) => (
